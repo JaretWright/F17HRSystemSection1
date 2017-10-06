087129108f0f846bfa3da5f6180c7a36c1ff8939
JaretWright <hkkA223>
2017-10-06 10:51:49 -0400

    Added new employee view and controller

--- src/views/CreateEmployeeViewController.java
+++ src/views/CreateEmployeeViewController.java
@@ -3,8 +3,12 @@ package views;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import models.Employee;
+import models.HourlyEmployee;
 
 /**
  * FXML Controller class
@@ -15,13 +19,26 @@ public class CreateEmployeeViewController implements Initializable {
 
     private ObservableList<Employee> employees;
     
+    @FXML private TextField firstNameTextField;
+    @FXML private TextField lastNameTextField;
+    @FXML private TextField sinTextField;
+    @FXML private DatePicker dob;
     
     public void initialData(ObservableList<Employee> listOfEmp)
     {
         employees = listOfEmp;
     }
     
-    
+    public void createNewEmployeeButtonPushed()
+    {
+        HourlyEmployee newEmp = new HourlyEmployee(firstNameTextField.getText(), 
+                                                    lastNameTextField.getText(), 
+                                                    sinTextField.getText(), 
+                                                    dob.getValue());
+        System.out.println(newEmp);
+        employees.add(newEmp);
+    }
+            
     /**
      * Initializes the controller class.
      */
