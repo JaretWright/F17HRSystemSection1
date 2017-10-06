97d6c1e5b615b025a3a5fe4023e2101c7ee21135
JaretWright <hkkA223>
2017-10-06 09:52:46 -0400

    Initial Upload

---
+++ src/views/AllEmployeeViewController.java
@@ -0,0 +1,23 @@
+
+package views;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author JWright
+ */
+public class AllEmployeeViewController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
